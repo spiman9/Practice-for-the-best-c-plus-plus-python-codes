f2c435778cdd86942e3c8fbf71c41fe3fa65c34a
Pritham Baswani Girylkar <108477555+spiman9@users.noreply.github.com>
2023-08-14 15:08:24 +0530

    c2

--- C++ Classes/OverloadInsertionExtraction.cpp
+++ C++ Classes/OverloadInsertionExtraction.cpp
@@ -14,7 +14,24 @@ class Complex{
         this->img = img;
     }
 
+
     void printHeading();
+
+    //overoad the  < operator BY MEMBER FUNCTION
+    bool operator < (Complex c){
+        if(real < c.real){
+            return true;
+        }
+        return false;
+    }
+
+    // OERVLOADING THE GREATER THAN BY FRIEND FUNCTION
+    friend bool operator > (Complex c1 , Complex c2){
+        if(c1.real > c2.real){
+            return true;
+        }
+        return false;
+    }
 
 };
 
@@ -24,11 +41,14 @@ void Complex::printHeading(){
 }
 
 
+// OVERLOADING THE INSERTION OPERATOR
 ostream& operator << (ostream& out , Complex &c){
     out << c.img << "i + " << c.real << endl;
     return out;
 }
 
+
+// OVERLOADING THE EXTRATION OPERATOR
 istream& operator >> (istream& in , Complex&c){
     in >> c.real >> c.img;
     return in;
@@ -38,9 +58,21 @@ int main(){
 
     Complex c1;
     c1.printHeading();
-    
+
     cout << "Enter the real and img : " ;
     cin >> c1;
-    cout << "The roots are  : " << c1;
+    cout << "The number is  : " << c1;
+
+    Complex c2;
+    c2.printHeading();
+
+    cout << "Enter the real and img : " ;
+    cin >> c2;
+    cout << "The numebr is  : " << c2;
+
+
+
+    cout << (c1 < c2) << endl;
+    cout << (c1 > c2) << endl;
     return 0;
 }
